9e55f2105170471a9bc16b51aa50fa57034b3b07
Nikita <nikitamaskin802@gmail.com>
2024-03-02 10:49:31 +0500

    first commit2

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -194,32 +194,31 @@
             <a href="#" class="nav-link">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Dashboard
+                Главная
                 <i class="right fas fa-angle-left"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="../../index.html" class="nav-link">
+                <a href="#" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v1</p>
+                  <p>Категории</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="../../index2.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v2</p>
+                  <p>Список категорий</p>
                 </a>
               </li>
               <li class="nav-item">
                 <a href="../../index3.html" class="nav-link">
                   <i class="far fa-circle nav-icon"></i>
-                  <p>Dashboard v3</p>
+                  <p>Новая категория</p>
                 </a>
               </li>
             </ul>
           </li>
-         
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
